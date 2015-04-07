4e06cf89b7bade863fc70b4ec302f344e40fb883
smaran123 <katta04.bhagath@gmail.com>
2015-04-07 23:40:44 +0530

    generate link_scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150407180310) do
+
+  create_table "links", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "url",        limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
