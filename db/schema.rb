7413e61af85da2c29fc1603232f4dc1408633a12
smaran123 <katta04.bhagath@gmail.com>
2015-04-10 00:31:49 +0530

    added name to user model and views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409181754) do
+ActiveRecord::Schema.define(version: 20150409184656) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id",    limit: 4
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20150409181754) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
