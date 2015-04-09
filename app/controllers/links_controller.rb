dc5c75c9674a3d9aa1c1a3f86830433cb4ea8c7a
smaran123 <katta04.bhagath@gmail.com>
2015-04-09 22:27:00 +0530

    add structure&basic styling

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,14 +2,11 @@ class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, :except => [:index, :show]
   before_action :authorized_user, :only => [:edit,:update,:destroy]
-  # GET /links
-  # GET /links.json
+
   def index
     @links = Link.all
   end
 
-  # GET /links/1
-  # GET /links/1.json
   def show
   end
 
@@ -23,8 +20,7 @@ class LinksController < ApplicationController
   def edit
   end
 
-  # POST /links
-  # POST /links.json
+
   def create
     @link = current_user.links.build(link_params)
 
@@ -65,7 +61,6 @@ class LinksController < ApplicationController
 
   private
    
-  # Use callbacks to share common setup or constraints between actions.
   def authorized_user
     @link = current_user.links.find_by(id: params[:id])
     redirect_to links_path, notice: "Not authorized to edit this link" if @link.nil?
@@ -75,7 +70,6 @@ class LinksController < ApplicationController
     @link = Link.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def link_params
     params.require(:link).permit(:title, :url)
   end
